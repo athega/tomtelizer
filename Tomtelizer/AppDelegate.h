d25b89b4a706ecd08a546d15afb2be1124e4e6cc
Petter Petersson <petter.petersson@athega.se>
2011-12-07 13:44:17 +0100

    Initial Commit

---
+++ Tomtelizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tomtelizer
+//
+//  Created by Petter Petersson on 12/7/11.
+//  Copyright (c) 2011 Athega AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tomtelizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tomtelizer
+//
+//  Created by Petter Petersson on 12/7/11.
+//  Copyright (c) 2011 Athega AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
